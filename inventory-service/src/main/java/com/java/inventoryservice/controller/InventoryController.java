549737ee504710dd5938fbf6a9670578e9e0b8d3
Manikanta M <manikantapoojary444@gmail.com>
2022-06-18 15:34:41 +0530

    added resilience4j for fault tolerance

--- inventory-service/src/main/java/com/java/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/java/inventoryservice/controller/InventoryController.java
@@ -4,6 +4,7 @@ import com.java.inventoryservice.dto.InventoryRequest;
 import com.java.inventoryservice.dto.InventoryResponse;
 import com.java.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,10 +12,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/v0/api/inventory")
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryController {
     private final InventoryService inventoryService;
     @PostMapping()
-    public List<InventoryResponse> checkItemsInStock(@RequestBody InventoryRequest inventoryRequest){
+    public List<InventoryResponse> checkItemsInStock(@RequestBody InventoryRequest inventoryRequest) throws InterruptedException {
+        log.info("wait started");
+        Thread.sleep(10000);
+        log.info("wait ended");
         return (inventoryService.checkItemsInStock(inventoryRequest));
     }
 }
--- order-service/src/main/java/com/java/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/java/orderservice/controller/OrderController.java
@@ -3,13 +3,16 @@ package com.java.orderservice.controller;
 import com.java.orderservice.dto.OrderRequestDto;
 import com.java.orderservice.dto.OrderResonseDTO;
 import com.java.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeoutException;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +21,22 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<String> placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
-        return new ResponseEntity<>(orderService.placeOrder(orderRequestDto), HttpStatus.CREATED);
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallBackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequestDto orderRequestDto) {
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequestDto));
     }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CompletableFuture<String> fallBackMethod(OrderRequestDto orderRequestDto,RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()->"Something went wrong please try again in sometime");
+    }
+
+//    @ResponseStatus(HttpStatus.REQUEST_TIMEOUT)
+//    public CompletableFuture<String> fallBackMethod(OrderRequestDto orderRequestDto, TimeoutException timeoutException){
+//        return CompletableFuture.supplyAsync(()->"time out error");
+//    }
+
 }
